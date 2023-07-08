b88f0ca18b6d566b603f45516c2bf8615c23b320
Alexey Shary <badirbis@yandex.ru>
2023-07-08 17:23:56 +0300

    refactor: Применен @Builder для User, UserDto, Item, ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,11 +1,13 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
 public class ItemDto {
     private Long id;
     @NotBlank(message = "Имя не должно быть пустым")
