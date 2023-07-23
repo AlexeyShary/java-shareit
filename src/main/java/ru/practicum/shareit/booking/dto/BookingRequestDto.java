80ff1717654d2dbadc24b0271b53c7fdc388bf4e
Alexey Shary <badirbis@yandex.ru>
2023-07-23 10:34:12 +0300

    fix: Фикс валидации start + end time BookingRequestDto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
@@ -19,14 +19,9 @@ public class BookingRequestDto {
     @Future(message = "Время окончания должно быть в будущем")
     private LocalDateTime end;
 
-    @AssertTrue(message = "Время окончания не может быть до старта")
+    @AssertTrue(message = "Время окончания должно быть после старта")
     private boolean isEndAfterStart() {
         return start == null || end == null || end.isAfter(start);
-    }
-
-    @AssertTrue(message = "Время старта и окончания не должны быть равны")
-    private boolean isStartEqualsEnd() {
-        return start == null || !start.equals(end);
     }
 
     @NotNull(message = "ID вещи не может быть пустым")
